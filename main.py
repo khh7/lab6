15b8a90b89fad48fd27651e42042d8dcd6cdfd39
khh7 <kim.monica@ufl.edu>
2023-03-21 14:44:54 -0400

    option 1: encoding and option 3: exit done

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 def encode(password):
 
-    password_list = list(password)
-    encoded_password_str = ""
+    password_list = list(password)  # turn int input into list
+    encoded_password_str = ""   # empty string to add new password to
 
     for element in password_list:
         element = int(element)
-        element += 3
-        if element > 9:
+        element += 3    # add three to each number
+        if element > 9: # if the added element is greater than 9, just take second value
             element %= 10
-        encoded_password_str += str(element)
+        encoded_password_str += str(element)    # concatenate to empty string
     return encoded_password_str
 
 def main():
