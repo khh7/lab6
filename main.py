5dff27530d403bee5ff692a1dbef5d8ab185949a
khh7 <kim.monica@ufl.edu>
2023-03-21 14:17:32 -0400

    encoding done

--- main.py
+++ main.py
@@ -1,9 +1,30 @@
-def encode():
-    pass
+def encode(password):
+
+    password_list = list(password)
+    encoded_password_str = ""
+
+    for element in password_list:
+        element = int(element)
+        element += 3
+        if element > 9:
+            element %= 10
+        encoded_password_str += str(element)
+    return encoded_password_str
 
 def main():
-    pass
 
+    print("Menu")
+    print("-------------")
+    print("1. Encode")
+    print("2. Decode")
+    print("3. Quit\n")
+
+    option = int(input("Please enter an option: "))
+
+    if option == 1:
+        og_password = input("Please enter your password to encode: ")
+        encode(og_password)
+        print("Your password has been encoded and stored!")
 
 if __name__ == "__main__":
     main()
